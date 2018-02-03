20221b8d63dbe383500b4579b8c7d1589bb7c749
mehmetaltuner <altuner17@itu.edu.tr>
2018-02-03 20:54:06 +0300

    Small changes

--- Photash/settings.py
+++ Photash/settings.py
@@ -164,7 +164,7 @@ EMAIL_PORT = 587
 
 REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 3
+    'PAGE_SIZE': 10
 }
 
 #Logged in userların redirect edileceği sayfa urli
