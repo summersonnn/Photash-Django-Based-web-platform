117b338e71773bf7b8480d56596d4db6be69ed86
mehmetaltuner <altuner17@itu.edu.tr>
2018-02-04 19:48:18 +0300

    deleted my changes in star_ratings

--- star_ratings/views.py
+++ star_ratings/views.py
@@ -31,8 +31,6 @@ class Rate(View):
             score = data.get('score')
             user = request.user.is_authenticated and request.user or None
             try:
-                if score == 0:
-                    return JsonResponse(data={'error': 'error'}, status=400)
                 rating = self.model.objects.rate(self.get_object(), score, user=user, ip=ip)
 
                 if request.is_ajax():
