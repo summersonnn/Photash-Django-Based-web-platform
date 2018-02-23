b990ac077e46befd6b014e05da7ab94f147796fa
SUMMERSON <kyazogluu@gmail.com>
2018-02-23 21:38:41 +0300

    new homepage

--- static/js/main-script.js
+++ static/js/main-script.js
@@ -1,11 +1,10 @@
 /*----------------------------------------------
-    Card Slider function call
+    Initial Functions 
 ------------------------------------------------*/
-/*---------------------------------------------------
-   Table Script
-  --------------------------------------------------*/
 
   $(document).ready(function(){
+
+/*--------- Compare Table function ---------*/
     $('.comp-table tr>td:first-child').hover(function(){
         $('.features-title .title-height-add').css('top', '-20px');
         $('.premium-title .title-height-add').css('top', '0px');
@@ -25,17 +24,16 @@
         $('.premium-title .title-height-add').css('top', '-20px');
       }
     );
+/*--------- ScrollingEffects Options ---------*/
+
+    var options = {
+        animationSpeed : 900,
+        autoHash: false,
+        headerHash: false
+      };
+      $.scrollingeffect(options);
 
   });
-
-  /****************************************************/
-
-$(document).ready(function(){
-  $(".owl-carousel").owlCarousel();
-});
-
-
-
 
 /*---------------------------------------------------
     Cards Options
@@ -58,25 +56,64 @@ $('.card-slider').owlCarousel({
 });
 
 /*---------------------------------------------------
-  Options for the scrolling effect
-  --------------------------------------------------*/
-
-  $(document).ready(function(){
-      var options = {
-        animationSpeed : 900,
-        autoHash: false,
-        headerHash: false
-      };
-      $.scrollingeffect(options);
-  });
-
-  /*---------------------------------------------------
   Line height of the arrows
-  --------------------------------------------------*/
+--------------------------------------------------*/
 
   $(document).ready(setInterval(function(){
       var cardHeight = $('.owl-item.active').height();
       $('.owl-next, .owl-prev').css('line-height', cardHeight+'px');
   },
-      100));
+  100));
+
+/*---------------------------------------------------
+  Anchors Animations/Scrolling
+--------------------------------------------------*/
+
+  $(document).ready(function(){
+
+   /* Tıklandığında smooth şekilde bir yere scroll etmek için*/
+    $('a[href^="#"]').on('click',function(e){
+          e.preventDefault();
+
+          var target = this.hash;
+          var $target = $(target);
+          //scroll
+           $('html, body').animate({
+            /*TEHLIKE!!! NAVBAR SORUN CIKARDIGI ICIN -79 PIXEL YUKARIYA SCROLL ETMESINI SAGLADIM. ILERDE UYUMLULUK SORUNLARI CIKABILIR*/
+            'scrollTop': $target.offset().top-79
+           },1000, 'swing');
+
+    });
+    /*--------------------------------
+
+    /*BACK TO THE TOP JQUERY*/
+    var btt = $('back-to-top');
+    btt.on('click', function(e){
+        $('html, body').animate({
+            scrollTop: 0
+        }, 1000);
+
+        e.preventDefault();
+    });
+
+    $(window).on('scroll', function() {
+
+        var self = $(this),
+            height = self.height();
+            top = self.scrollTop();
+         console.log(height);
+         console.log(top);
+
+        if (top > height)
+            if (!btt.is(':visible')) {
+                btt.show();
+            }
+            else {
+                btt.hide();
+            }
+    });
+    /*-----------------------------------------------*/
+});
+
+
  
