46abbf55daa57c05245247c3e1fd0c80f13d2303
mehmetaltuner <altuner17@itu.edu.tr>
2018-03-01 17:36:38 +0300

    Recommendation algo 1

--- contest/api/urls.py
+++ contest/api/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import ContestListAPIView, ContestDetailAPIView, VotersListAPIView, FeedAPIView
+from .views import ContestListAPIView, ContestDetailAPIView, VotersListAPIView, FeedAPIView, AskForTags
 
 app_name = 'contest_api'
 
@@ -8,4 +8,5 @@ urlpatterns = [
     path('<slug:slug>/detail/', ContestDetailAPIView.as_view(), name='detail'),
     path('<slug:slug>/voters/', VotersListAPIView.as_view(), name='voters'),
     path('timeline/', FeedAPIView.as_view(), name='timeline'),
+    path('ask-for-tags/', AskForTags.as_view(), name='ask_for_tags'),
 ]
