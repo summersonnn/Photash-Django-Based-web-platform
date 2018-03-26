6501b4a6923b979fb838279d35b86f5f30ff0973
summersonnn <yazogluk@itu.edu.tr>
2018-03-26 12:56:02 +0300

    Catalogue added. Backend is not finished

--- contest/api/urls.py
+++ contest/api/urls.py
@@ -8,7 +8,7 @@ urlpatterns = [
     path('<slug:slug>/detail/', ContestDetailAPIView.as_view(), name='detail'),
     path('<slug:slug>/voters/', VotersListAPIView.as_view(), name='voters'),
     path('<slug:slug>/rankings/', ContestRankingAPIView.as_view(), name='rankings'),
-    path('timeline/', FeedAPIView.as_view(), name='timeline'),
+    path('catalogue/', FeedAPIView.as_view(), name='catalogue'),
     path('ask-for-tags/', AskForTags.as_view(), name='ask_for_tags'),
     path('tag/<slug:slug>/', TagDetailAPIView.as_view(), name='tag'),
 ]
