64ffb0040e9940af062c41f059427de232b1b968
summersonnn <yazogluk@itu.edu.tr>
2018-03-27 22:50:35 +0300

    Photopool has subfolders with the name of contest slugs

--- photo/models.py
+++ photo/models.py
@@ -6,12 +6,14 @@ from django.db.models.signals import pre_delete  #R eceive the pre_delete signal
 from django.dispatch.dispatcher import receiver
 import os
 
+def get_upload_path(instance, filename):
+    return 'photopool/contest_{0}/{1}'.format(instance.contest.slug, filename)
 
 # Create your models here.
 class Photo(models.Model):
     # Aynı fotoğraf farklı contestlerde farklı id'ye sahip olacağından tek primary key photoid (belki bunu değiştirebilirim)
     id = models.IntegerField(primary_key=True, verbose_name='Photo id')
-    photoItself = models.ImageField(upload_to='photopool/', default='blog/static/manzara.jpg', verbose_name='Photo')
+    photoItself = models.ImageField(upload_to=get_upload_path, default='blog/static/manzara.jpg', verbose_name='Photo')
     ownername = models.ForeignKey('auth.User', verbose_name='Name of the owner of the photo', on_delete=models.CASCADE, related_name='photos')
     contest = models.ForeignKey('contest.Contest', verbose_name='Contest name', on_delete=models.CASCADE, related_name='photos')
     likes = models.ManyToManyField ('auth.User', blank=True, related_name="photo_likes")
