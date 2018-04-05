b43e6497d8df3d21837c24b3ccd18595f2d5dfc9
summersonnn <yazogluk@itu.edu.tr>
2018-04-05 13:32:58 +0300

    Captcha v1 removed. V2 will be added soon

--- contest/forms.py
+++ contest/forms.py
@@ -1,10 +1,8 @@
 from django import forms
 from photo.models import *
-from captcha.fields import ReCaptchaField
 
 
 class PhotoForm(forms.ModelForm):
-    captcha = ReCaptchaField()
 
     class Meta:
         model = Photo
