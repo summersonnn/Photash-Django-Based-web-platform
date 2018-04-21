1af29ea285209b06f7ef0ba50004ead5dfaf024e
summesonnn <kyazogluu@gmail.com>
2018-04-21 13:02:01 +0300

    Placeholder fix

--- contest/forms.py
+++ contest/forms.py
@@ -6,7 +6,7 @@ from snowpenguin.django.recaptcha2.widgets import ReCaptchaWidget
 
 class PhotoForm(forms.ModelForm):
     captcha = ReCaptchaField(widget=ReCaptchaWidget())
-    photo_caption = forms.CharField(max_length=90, widget=forms.TextInput(attrs={'placeholder': 'Search'}))
+    photo_caption = forms.CharField(max_length=90, widget=forms.TextInput(attrs={'placeholder': 'Enter your caption here'}))
     class Meta:
         model = Photo
         fields = [
